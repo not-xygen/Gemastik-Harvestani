f99d970658c6578f683f41242893d04545dc6700
Muhammad Fathurrohim <m.faturrohim13@gmail.com>
2023-07-07 21:13:23 +0800

    feat(server): add default error handler for typia

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,7 +3,9 @@ import cors from "cors";
 
 import * as auth from "#/route/api/v1/auth.js";
 import { injectUser } from "#/middleware/auth.js";
-import parseCookie from "cookie-parser"
+import parseCookie from "cookie-parser";
+import { NextFunction, Request, Response } from "express-serve-static-core";
+import { TypeGuardError } from "typia";
 
 const app = express();
 
@@ -13,6 +15,15 @@ app.use(injectUser);
 app.use(parseCookie());
 
 app.use("/api/v1/auth", auth.router());
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof TypeGuardError) {
+    res.status(422).json({ error: error.message });
+  } else {
+    console.log(error);
+    next();
+  }
+});
 
 const port = process.env.APP_PORT;
 app.listen(port, () => {
