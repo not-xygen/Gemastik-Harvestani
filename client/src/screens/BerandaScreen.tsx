31f1ccb64db861dca009238d243b9ee0b057cf55
ansxy <ansarfadillah17@gmail.com>
2023-07-16 05:46:52 +0800

    feat : itterate all lahan into card

--- client/src/screens/BerandaScreen.tsx
+++ client/src/screens/BerandaScreen.tsx
@@ -1,6 +1,9 @@
 import { StyleSheet, Text, View, ScrollView, Image, ImageBackground } from 'react-native'
 
 import { StatusBar } from 'expo-status-bar'
+import { useLahanContext } from '@/hooks/LahanHooks'
+import { useEffect } from 'react'
+import { LahanCard } from '@/components'
 
 const styles = StyleSheet.create({
 	container: {
@@ -76,6 +79,10 @@ const styles = StyleSheet.create({
 })
 
 export default function BerandaScreen() {
+	const {show,allLahan} = useLahanContext()
+	useEffect(() => {
+		show()
+	},[])
 	return (
 		<View style={styles.container}>
 			<View style={styles.headerContainer}>
@@ -157,140 +164,12 @@ export default function BerandaScreen() {
 					Lahan-mu
 				</Text>
 				<ScrollView style={{ height: '100%', paddingHorizontal: 20, paddingVertical: 12 }}>
-					<View
-						style={{
-							display: 'flex',
-							backgroundColor: '#FAFAFA',
-							flexDirection: 'row',
-							alignItems: 'center',
-							width: '100%',
-							borderRadius: 8,
-							gap: 8,
-							shadowColor: '#E86A33',
-							shadowOffset: {
-								width: 0,
-								height: 0,
-							},
-							shadowOpacity: 0.5,
-							shadowRadius: 1,
-							elevation: 10,
-							marginBottom: 12,
-						}}
-					>
-						<Image
-							source={require('../../assets/placeholder_background.jpg')}
-							style={{ width: '20%', height: 80, borderRadius: 8 }}
-						/>
-						<View
-							style={{
-								display: 'flex',
-								flexGrow: 1,
-								flexDirection: 'row',
-								alignItems: 'center',
-								justifyContent: 'space-between',
-							}}
-						>
-							<View
-								style={{
-									display: 'flex',
-									flexDirection: 'column',
-									alignItems: 'flex-start',
-									gap: 4,
-								}}
-							>
-								<Text style={{ fontSize: 16, fontWeight: '700' }}>Lahan 1</Text>
-								<View
-									style={{
-										backgroundColor: '#E86A33',
-										paddingHorizontal: 8,
-										paddingVertical: 4,
-										borderRadius: 8,
-									}}
-								>
-									<Text
-										style={{
-											fontSize: 12,
-											color: '#FAFAFA',
-											fontWeight: '400',
-										}}
-									>
-										Jagung
-									</Text>
-								</View>
-							</View>
-							<Image
-								source={{ uri: 'https://img.icons8.com/ios/50/forward--v1.png' }}
-								style={{ height: 30, width: 30, marginRight: 12 }}
-							/>
-						</View>
-					</View>
-					<View
-						style={{
-							display: 'flex',
-							backgroundColor: '#FAFAFA',
-							flexDirection: 'row',
-							alignItems: 'center',
-							width: '100%',
-							borderRadius: 8,
-							gap: 8,
-							shadowColor: '#E86A33',
-							shadowOffset: {
-								width: 0,
-								height: 0,
-							},
-							shadowOpacity: 0.5,
-							shadowRadius: 1,
-							elevation: 10,
-							marginBottom: 12,
-						}}
-					>
-						<Image
-							source={require('../../assets/placeholder_background.jpg')}
-							style={{ width: '20%', height: 80, borderRadius: 8 }}
-						/>
-						<View
-							style={{
-								display: 'flex',
-								flexGrow: 1,
-								flexDirection: 'row',
-								alignItems: 'center',
-								justifyContent: 'space-between',
-							}}
-						>
-							<View
-								style={{
-									display: 'flex',
-									flexDirection: 'column',
-									alignItems: 'flex-start',
-									gap: 4,
-								}}
-							>
-								<Text style={{ fontSize: 16, fontWeight: '700' }}>Lahan 1</Text>
-								<View
-									style={{
-										backgroundColor: '#E86A33',
-										paddingHorizontal: 8,
-										paddingVertical: 4,
-										borderRadius: 8,
-									}}
-								>
-									<Text
-										style={{
-											fontSize: 12,
-											color: '#FAFAFA',
-											fontWeight: '400',
-										}}
-									>
-										Jagung
-									</Text>
-								</View>
-							</View>
-							<Image
-								source={{ uri: 'https://img.icons8.com/ios/50/forward--v1.png' }}
-								style={{ height: 30, width: 30, marginRight: 12 }}
-							/>
-						</View>
-					</View>
+					{!allLahan ? <>
+						</> : <>
+						{allLahan.lahan.map((data : any) => {
+								return <LahanCard nama={data.nama} />
+							})}</>}
+					
 				</ScrollView>
 			</View>
 		</View>
