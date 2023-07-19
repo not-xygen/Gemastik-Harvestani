dca8e367db15b76faf1d6d27030b334af0f787b7
ansxy <ansarfadillah17@gmail.com>
2023-07-20 06:44:21 +0800

    feat : add lahan detail page and refactoring the router

--- client/src/layout/NavbarLayout.tsx
+++ client/src/layout/NavbarLayout.tsx
@@ -5,9 +5,17 @@ import {
 	ManajemenLahanScreen,
 	ManajemenKeuanganScreen,
 	ProfilScreen,
+    LahanDetailScreen,
+    LahanDetail,
 } from '@/screens'
+import { Settings } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { LahanCard } from "@/components";
+import { BottomSheetModalProvider } from "@gorhom/bottom-sheet/lib/typescript/contexts";
 
 const Tab = createBottomTabNavigator()
+const LahanStack = createNativeStackNavigator()
+const BerandaStack = createNativeStackNavigator()
 export default function NavbarLayout () {
     return (
         <LahanProvider>
@@ -17,8 +25,30 @@ export default function NavbarLayout () {
                 "paddingTop" : 10,
                 "paddingBottom" : 10,
             }}} initialRouteName="Beranda">
-                    <Tab.Screen name="Beranda" component={BerandaScreen} />
-                    <Tab.Screen name="Lahan" component={ManajemenLahanScreen} />
+                    <Tab.Screen name="Beranda" >
+                        {() => (
+                            <BerandaStack.Navigator screenOptions={{headerShown : false}}>
+                                <BerandaStack.Screen name="Beranda" component={BerandaScreen}/>
+                                <BerandaStack.Screen name="LahanDetail">
+                                    {()=>(
+                                        <LahanDetail/>
+                                    )}
+                                </BerandaStack.Screen>
+                            </BerandaStack.Navigator>
+                        )}
+                    </Tab.Screen>
+                    <Tab.Screen name="Lahan" >
+                        {() => (
+                            <LahanStack.Navigator screenOptions={{headerShown : false}}>
+                                <LahanStack.Screen name="ManajemenLahanScreen" component={ManajemenLahanScreen} />
+                                <LahanStack.Screen name="LahanDetail">
+                                    {()=>(
+                                        <LahanDetail/>
+                                    )}
+                                </LahanStack.Screen>
+                            </LahanStack.Navigator>
+                        )}
+                    </Tab.Screen>
                     <Tab.Screen name="Keuangan" component={ManajemenKeuanganScreen} />
                     <Tab.Screen name="Profil" component={ProfilScreen} />
             </Tab.Navigator>
